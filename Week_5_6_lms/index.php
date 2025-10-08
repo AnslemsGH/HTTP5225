a01bce374f1d2e93920ca35c4e4da965d38c2cd4
Anslem Coelho <anslem1999@gmail.com>
2025-10-08 15:54:01 -0400

    Week 6

---
+++ Week_5_6_lms/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Schools</title>
+</head>
+<body>
+    <h1>Schools</h1>
+    <div>
+        <?php include('nav.php') ?>
+    </div>
+    <hr>
+    <div>
+        <?php
+
+        require('connect.php');
+        
+        $query = 'SELECT * FROM schools';
+
+        $schools = mysqli_query($connect,$query);
+
+        // echo '<pre>'.print_r($schools).'</pre>'
+
+        foreach($schools as $school){
+            echo $school['School Name'] . 
+            '<form>
+            <input type="hidden" name="id" value="123">
+            <input type="submit" value="Edit">
+            </form>'
+            .'<br>';
+        };
+
+
+        ?>
+        <style>
+            .Box{
+                border: 1px solid red;
+
+            }
+        </style>
+        <div class="Box">
+            <h3>School Name</h3>
+        </div>
+    </div>
+</body>
+</html>
---
+++ Week_5_6_lms/nav.php
@@ -0,0 +1,6 @@
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li><a href="addschool.php">Add School</a></li>
+            </ul>
+        </nav>
