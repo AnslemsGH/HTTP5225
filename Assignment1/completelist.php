9c313a26cd500eec0b638e90ed822b7922817db9
Anslem Coelho <anslem1999@gmail.com>
2025-10-21 18:14:42 -0400

    Working Logic

--- Assignment1/completelist.php
+++ Assignment1/completelist.php
@@ -1,28 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Permits</title>
+</head>
+<body>
+    
     <h1 style="text-align: center;">Toronto Noise Exemption Permits</h1>
 
     <div>
         <?php include('nav.php'); ?>
 
     </div>
-<?php
+    <?php
 
-    require('connect.php');
-        
-    $query = 'SELECT * FROM permits';
+        require('connect.php');
+            
+        $query = 'SELECT * FROM permits';
 
-    $permit = mysqli_query($connect,$query);
+        $permit = mysqli_query($connect,$query);
 
-    foreach($permit as $permit) {
+        foreach($permit as $permit) {
 
-        $clientname = $permit['client_name'];
-
-
-        echo '<a href="permit.php" 
-                style="text-decoration: none; font-size: 1.1rem; font-weight: 500; transition: color 0.3s ease;">'
-                . $clientname . '
-            </a><br>-----<br>';
-
-    }
+            $clientname = $permit['client_name'];
 
 
-?>
+            echo  $clientname . '
+                <form action="permit.php">
+                    <input type="hidden" name="_id" value="' . $permit['_id'] . '">
+                    <button type="submit" class="btn btn-sm btn-primary">View Permit</button>
+                </form>
+                
+                <br>-----<br>';
+
+        }
+
+
+    ?>
--- Assignment1/permit.php
+++ Assignment1/permit.php
@@ -1,23 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Individual Permits</title>
+</head>
+<body>
+        
     <h1 style="text-align: center;">Toronto Noise Exemption Permits</h1>
 
-    <div>
-        <?php include('nav.php'); ?>
+        <div>
+            <?php include('nav.php'); ?>
+        </div>
 
-    </div>
+    <?php
 
-<?php
+    require('connect.php');
 
+            if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['_id'])) {
+                $id = $_GET['_id'];
+                $query = "SELECT * FROM permits WHERE _id=$id";
+                $result = mysqli_query($connect, $query);
+                $permit = $result->fetch_assoc();
 
-require('connect.php');
-
-        
-        $query = 'SELECT * FROM permits';
-
-        $permit = mysqli_query($connect,$query);
-
-echo '<h2 class="card-title">' . $permit['licence_number'] . '</h2>
-            <h2 class="card-title">Permit Type: ' . $permit['permit_type'] . '</h2>
+            echo '<h2 class="card-title">Permit Type: ' . $permit['permit_type'] . '</h2>
             <p class="card-text">Operating Name: ' . $permit['operating_name'] . '</p>
             <p class="card-text">Ward: ' . $permit['ward'] . '</p>
             <span class="badge bg-secondary">Client Name: ' . $permit['client_name'] . '</span>
-            <br>------------------------------------';
+            <br>------------------------------------'; 
+            }
+
+
+
